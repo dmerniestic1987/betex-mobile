8be6df5b8dc97e3d6ad0b0fc8bae770fdc43fa9b
dmerniestic1987 <dmernies@uade.edu.ar>
2019-06-02 19:06:56 -0300

    Se agregan depednencias de spongycastle, una implementación de Bouncy Caslte para poder utilizar web3j y provee implementación de JCE

---
+++ android/app/src/main/java/ar/com/betex/betexmobile/activities/WalletDevelActivity.java
@@ -0,0 +1,58 @@
+package ar.com.betex.betexmobile.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+import ar.com.betex.betexmobile.Exception.BetexWalletException;
+import ar.com.betex.betexmobile.R;
+import ar.com.betex.betexmobile.blockchain.BetexWallet;
+import ar.com.betex.betexmobile.blockchain.FileBetexWallet;
+
+/**
+ * Actividad de desarrollo
+ */
+public class WalletDevelActivity extends AppCompatActivity {
+    private static final String TAG = "WalletDevelActivity";
+    private BetexWallet betexWallet;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wallet_devel);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        betexWallet = FileBetexWallet.getInstance(this);
+
+        if (!betexWallet.isWalletCreated()){
+            try {
+                betexWallet.createWallet("wallet1", "clave001");
+
+            } catch (BetexWalletException e) {
+                Log.e(TAG, e.getMessage(), e);
+            }
+        }
+
+        else {
+            try {
+                betexWallet.loadWallet("clave001");
+            } catch (BetexWalletException e) {
+                Log.e(TAG, e.getMessage(), e);
+            }
+        }
+    }
+}
--- android/app/src/main/java/ar/com/betex/betexmobile/blockchain/FileBetexWallet.java
+++ android/app/src/main/java/ar/com/betex/betexmobile/blockchain/FileBetexWallet.java
@@ -5,12 +5,14 @@ import android.util.Log;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.web3j.crypto.Credentials;
 import org.web3j.crypto.WalletUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.security.Security;
 
 import ar.com.betex.betexmobile.Exception.BetexWalletException;
 import ar.com.betex.betexmobile.entities.WalletBean;
@@ -21,17 +23,31 @@ import ar.com.betex.betexmobile.entities.WalletBean;
  * @author Diego Mernies
  */
 public class FileBetexWallet implements BetexWallet{
+
     private static final String TAG = "FileBetexWallet";
     private static final String BETEX_WALLET_FILE_NAME="betexWallet.json";
     private static final String KEYSTORE_FOLDER = "keystore";
 
     private static Credentials credentials = null;
     private static WalletBean walletBean = null;
+    private static FileBetexWallet instance = null;
     private Context context;
 
     private FileBetexWallet(Context context) {
         super();
         this.context = context;
+    }
+
+    /**
+     * Obtiene una instancia de la wallet
+     * @param contexto
+     * @return
+     */
+    public static FileBetexWallet getInstance(Context contexto){
+        if (instance == null)
+            instance = new FileBetexWallet(contexto);
+
+        return instance;
     }
 /*
        public void importWallet(String name,String password,String privateKey,String filepath){
@@ -72,6 +88,7 @@ public class FileBetexWallet implements BetexWallet{
     }
     */
 
+
     @Override
     public boolean isWalletCreated() {
         File keyStoreDir = new File(keyStoreDirPath());
@@ -91,6 +108,7 @@ public class FileBetexWallet implements BetexWallet{
             BetexWalletException bex = new BetexWalletException("Betex Password no pude ser null");
             bex.setCode("loading_betex_wallet_file");
             bex.setOperation("Reading Wallet with null betexPassword");
+            throw bex;
         }
 
         //Cargar el archivo de la wallet
@@ -100,12 +118,12 @@ public class FileBetexWallet implements BetexWallet{
         }
 
         WalletBean tempWallet = this.readBetexWalletFile(betexWallet);
-        String betexPasswordSha256 = DigestUtils.sha256Hex(betexPassword);
+        String betexPasswordSha256 = new String(Hex.encodeHex(DigestUtils.sha256(betexPassword)));
         if(!tempWallet.getSha256BetexPassword().equals(betexPasswordSha256)){
             BetexWalletException bex = new BetexWalletException("Betex Password incorrecto");
             bex.setCode("loading_betex_wallet_file");
             bex.setOperation("Incorrect Betex credentials");
-            return false;
+            throw bex;
         }
 
         readWeb3jWalletCredentials(betexPassword, tempWallet.getWeb3jWalletFilePath());
@@ -118,20 +136,28 @@ public class FileBetexWallet implements BetexWallet{
     public void createWallet(String betexWalletName, String betexPassword) throws BetexWalletException {
         Log.i(TAG, "Creating Wallet");
 
+        //Primero creamos el archivo
         String web3jWalletFileName = null;
+        boolean okCreation = true;
         try {
+            File keyStorDir = new File(this.keyStoreDirPath());
+            if (!keyStorDir.exists()){
+                keyStorDir.mkdir();
+            }
             web3jWalletFileName = WalletUtils.generateLightNewWalletFile(betexPassword,new File(keyStoreDirPath()));
         } catch (Exception e) {
             BetexWalletException bex = new BetexWalletException("Error creating web3j wallet", e);
             bex.setCode("creating_betex_wallet_file");
             bex.setOperation("Creating a new web3j wallet");
+            throw bex;
         }
 
+        //Ahora leemos el archivo para cargar las nuevas credenciales generadas
         readWeb3jWalletCredentials(betexPassword, web3jWalletFileName);
 
         walletBean = new WalletBean();
         walletBean.setBetexName(betexWalletName);
-        String betexPasswordSha256 = DigestUtils.sha256Hex(betexPassword);
+        String betexPasswordSha256 = new String(Hex.encodeHex(DigestUtils.sha256(betexPassword)));
         walletBean.setSha256BetexPassword(betexPasswordSha256);
         walletBean.setAddress(credentials.getAddress());
         walletBean.setWeb3jWalletFilePath(web3jWalletFileName);
@@ -197,6 +223,7 @@ public class FileBetexWallet implements BetexWallet{
             BetexWalletException bex = new BetexWalletException("Error writing Betex Wallet File", e);
             bex.setCode("writing_betex_wallet_file");
             bex.setOperation("Writing JSON file in KeyStore");
+            throw bex;
         }
     }
 
@@ -213,6 +240,7 @@ public class FileBetexWallet implements BetexWallet{
             BetexWalletException bex = new BetexWalletException("Error creating web3j wallet", e);
             bex.setCode("creating_betex_wallet_file");
             bex.setOperation("Loading the new web3j wallet");
+            throw bex;
         }
     }
 }
